3a76e9667b6c9cfb1b37452cb25e5d8ae108bdbe
vivek286 <solankivivek01@gmail.com>
2021-07-29 09:44:46 +0530

    added css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-// import '../styles/globals.css'
+import tottal from '../styles/globals.css'
 import Layout from '../components/layout/Layout'
 function MyApp({ Component, pageProps }) {
-  return <Layout><Component {...pageProps} /></Layout>
+  return <tottal><Layout><Component {...pageProps} /></Layout></tottal>
 }
 
 export default MyApp
